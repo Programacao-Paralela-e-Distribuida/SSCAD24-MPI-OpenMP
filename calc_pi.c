ba0054abf167be868988a70e37e5b965beb18e99
Gabriel P. Silva <gabriel.silva@gmail.com>
2024-10-17 09:21:10 -0300

    Create calc_pi.c

---
+++ calc_pi.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <omp.h>
+#include <math.h>
+
+int main() {
+  long int num_steps = 10000000000;
+  double step = 1.0 / (double) num_steps;
+  double pi = 0.0, begin, end;
+  begin=omp_get_wtime();
+  {
+    for (long int i = 0; i < num_steps; i++) {
+      double x = (i + 0.5) * step;
+      pi += 4.0 / (1.0 + x * x);
+    }
+  }
+  pi *= step;
+  end=omp_get_wtime();
+  printf("Valor de Pi calculado: %3.15f. O tempo de execução foi %3.15f \n", pi, end-begin);
+
+  return 0;
+}
