d66fc05e7373e27d22837c748b1ac2d003d76464
Gabriel P. Silva <gabriel.silva@gmail.com>
2024-10-18 09:13:44 -0300

    Update calc_pi.c

--- calc_pi.c
+++ calc_pi.c
@@ -1,22 +1,20 @@
-// Compilar com opção para OpenMP para medida do tempo de execução
 #include <stdio.h>
-#include <omp.h>
-#include <math.h>
-
-int main() {
-  long int num_steps = 10000000000;
-  double step = 1.0 / (double) num_steps;
-  double pi = 0.0, begin, end;
-  begin=omp_get_wtime();
-  {
-    for (long int i = 0; i < num_steps; i++) {
-      double x = (i + 0.5) * step;
-      pi += 4.0 / (1.0 + x * x);
+#include <time.h>
+static long num_steps = 10000000000;
+double step;
+int main(int argc, char *argv[]) {
+    long int i;
+    double x, pi, sum = 0.0;
+    clock_t start_time, end_time;
+    step = 1.0 / (double)num_steps;
+    start_time = clock(); // Tempo de início da execução
+    for (i = 0; i < num_steps; i++) {
+        x = (i + 0.5) * step;
+        sum += 4.0 / (1.0 + x * x);
     }
-  }
-  pi *= step;
-  end=omp_get_wtime();
-  printf("Valor de Pi calculado: %3.15f. O tempo de execução foi %3.15f \n", pi, end-begin);
-
-  return 0;
+    pi = step * sum;
+    end_time = clock(); // Tempo final
+    double run_time = ((double)(end_time - start_time)) / CLOCKS_PER_SEC; // Tempo em segundos
+    printf("pi = %2.15f, %ld passos, computados em %lf segundos\n", pi, num_steps, run_time);
+    return 0;
 }
