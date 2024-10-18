e57eb31d24b49c0f643326f4e57db51dfaa59959
Gabriel P. Silva <gabriel.silva@gmail.com>
2024-10-18 09:24:55 -0300

    Update omp_off_calc_pi.c

--- omp_off_calc_pi.c
+++ omp_off_calc_pi.c
@@ -7,9 +7,7 @@ int main() {
   double step = 1.0 / (double) num_steps;
   double pi = 0.0;
   double begin, end;
-
   begin = omp_get_wtime();
-
   #pragma omp target data map(tofrom: pi) map(to:num_steps, step) device(1)// Diretiva para offloading para a GPU
   #pragma omp target teams distribute parallel for reduction(+:pi) // Paralelização com OpenMP
      for (long int i = 0; i < num_steps; i++) {
