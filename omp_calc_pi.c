15eb01ccd399bcbc76367daa74b59212e3ebb63d
Gabriel P. Silva <gabriel.silva@gmail.com>
2024-10-18 09:22:14 -0300

    Update omp_calc_pi.c

--- omp_calc_pi.c
+++ omp_calc_pi.c
@@ -3,22 +3,20 @@
 #include <math.h>
 
 int main() {
-  long int num_steps = 10000000000;
+  static long num_steps = 10000000000;
   double step = 1.0 / (double) num_steps;
-  double pi = 0.0, begin, end;
+  double sum=0.0, pi = 0.0, begin, end;
   begin=omp_get_wtime();
-  #pragma omp parallel shared(pi) num_threads(8)
+  #pragma omp parallel shared(sum, step, num_steps) num_threads(8)
   {
-   #pragma omp for reduction(+:pi)
+   #pragma omp for reduction(+:sum)
     for (long int i = 0; i < num_steps; i++) {
       double x = (i + 0.5) * step;
-      pi += 4.0 / (1.0 + x * x);
+      sum += 4.0 / (1.0 + x * x);
     }
   }
-  pi *= step;
-
+  pi = sum * step;
   end=omp_get_wtime();
   printf("Valor de Pi calculado: %3.15f. O tempo de execução foi %3.15f \n", pi, end-begin);
-
   return 0;
 }
