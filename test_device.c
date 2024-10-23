4823d710826df020262716973ac6c7bba10b42c9
Gabriel P. Silva <gabriel.silva@gmail.com>
2024-10-23 19:45:54 -0300

    Update test_device.c

--- test_device.c
+++ test_device.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <omp.h>
-
+//  Use este programa para verficar se as opções de compilação estão corretas 
+//  e a(s) GPUS(s) conseguem ser identificadas.
 int main() {
   int numdevices = omp_get_num_devices();
   int device= omp_get_device_num();
